2825919e17e935e757a93edcd58fae5dd35ae000
Kareem Nassar <knassar89@gmail.com>
2020-07-27 13:32:21 -0700

    Using GANs to visualize sound.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use cgmath::prelude::*;
 use three::Object;
 use std::sync::mpsc::*;
-use std::thread;
-use dasp_ring_buffer as ring_buffer;
-use dasp_signal::{self as signal, Signal};
-use dasp_signal::rms::SignalRms;
-use minifb::{Key, Window, WindowOptions};
+use minifb::{ Window, WindowOptions};
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 const WIDTH: usize = 100;
 const HEIGHT: usize = 1024;
@@ -20,114 +18,29 @@ fn main() {
     let cam = win.factory.perspective_camera(75.0, 1.0 .. 50.0);
     cam.set_position([0.0, 0.0, 10.0]);
 
-    // let hemisphere_light = win.factory.hemisphere_light(0xffffff, 0x8080ff, 0.5);
     let ambient_light = win.factory.ambient_light(0xffffffff, 0.5);
     let point_light = win.factory.point_light(0xffffff, 0.7);
     point_light.set_position([15.0, 35.0, 35.0]);
 
-    // let mut dir_light = win.factory.directional_light(0xffffff, 0.9);
-    // dir_light.look_at([15.0, 35.0, 35.0], [0.0, 0.0, 2.0], None);
-    // let shadow_map = win.factory.shadow_map(1024, 1024);
-    // let _debug_shadow = win.renderer
-    //     .debug_shadow_quad(&shadow_map, 1, [10, 10], [256, 256]);
-    // dir_light.set_shadow(shadow_map, 40.0, 1.0 .. 200.0);
-
     let lights: [&three::object::Base; 2] = [
-        // hemisphere_light.as_ref(),
         ambient_light.as_ref(),
         point_light.as_ref(),
-        // dir_light.as_ref(),
     ];
     for l in &lights {
         l.set_visible(true);
         win.scene.add(l);
     }
 
-    let mbox = {
-        let geometry = three::Geometry::cuboid(3.0, 2.0, 1.0);
-        let material = three::material::Phong { color: 0x00FF00, glossiness: 80.0, };
-        win.factory.mesh(geometry, material)
-    };
-    mbox.set_position([-3.0, -3.0, 0.0]);
-    // win.scene.add(&mbox);
-
-    let mcyl = {
-        let geometry = three::Geometry::cylinder(1.0, 2.0, 2.0, 5);
-        let material = three::material::Phong { color: 0xFF0000, glossiness: 80.0, };
-        win.factory.mesh(geometry, material)
-    };
-    mcyl.set_position([3.0, -3.0, 0.0]);
-    // win.scene.add(&mcyl);
-
-    let msphere = {
-        let geometry = three::Geometry::uv_sphere(2.0, 5, 5);
-        let material = three::material::Phong { color: 0xFF0000, glossiness: 80.0, };
-        win.factory.mesh(geometry, material)
-    };
-    msphere.set_position([-3.0, 3.0, 0.0]);
-    // win.scene.add(&msphere);
-
-    // win.scene.add(&mcyl);
-    // win.scene.add(&mbox);
-
-    let mline = {
-        let geometry = three::Geometry::with_vertices(vec![
-            [-2.0, -1.0, 0.0].into(),
-            [0.0, 1.0, 0.0].into(),
-            [2.0, -1.0, 0.0].into(),
-        ]);
-        let material = three::material::Phong { color: 0x0000FF , glossiness: 80.0,};
-        win.factory.mesh(geometry, material)
-    };
-    mline.set_position([3.0, 3.0, 0.0]);
-    // win.scene.add(&mline);
-
     let sphere_geometry = three::Geometry::uv_sphere(2.0, 32, 32);
     let mut dynamic = {
         let material = three::material::Wireframe{
             color: 0xFFFFFF,
         };
-        // let material = three::material::Pbr{
-        //     base_color_factor: 0xffA0A0,
-        //     base_color_alpha: 0.3,
-        //     metallic_factor: 1.0,
-        //     roughness_factor: 0.5,
-        //     occlusion_strength: 0.5,
-        //     emissive_factor: three::color::BLACK,
-        //     normal_scale: 1.0,
-        //     base_color_map: None,
-        //     normal_map: None,
-        //     emissive_map: None,
-        //     metallic_roughness_map: None,
-        //     occlusion_map: None,
-        // };
-        // let material = three::material::Phong {
-        //     color: 0xffA0A0,
-        //     glossiness: 80.0,
-        // };
-        // let material = three::material::Lambert {
-        //     color: 0xA0ffA0,
-        //     flat: false,
-        // };
         win.factory.mesh_dynamic(sphere_geometry, material)
     };
     dynamic.set_position([0.0, 0.0, 0.0]);
     win.scene.add(&dynamic);
 
-    let mut frame_buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-
-    let mut window = Window::new(
-        "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
-
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
     println!("vertex count: {}", dynamic.vertex_count());
 
@@ -147,18 +60,53 @@ fn main() {
     };
     let mut stream = pa.open_non_blocking_stream(input_settings, callback).expect("Unable to create stream"); 
     stream.start().expect("Unable to start stream");
-    let ring_buffer = ring_buffer::Fixed::from([0.0 as f32; 4096]);
 
-    let mut angle = cgmath::Rad::zero();
-    
+    let mut freq_frame_buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut freq_window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+    // // Limit to max ~60 fps update rate
+    freq_window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut gan_frame_buffer: Vec<u32> = vec![0; 512 * 512];
+
+    let mut gan_window = Window::new(
+        "gan window",
+        512,
+        512,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    gan_window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    let model = tch::CModule::load("./traced_portrait_dgan.pt").expect("could not load torch model");
+
+    let mut noise: Vec<f32> = vec![0.0; 100];
+
+    let mut iv: Vec<usize> = vec![0; 100];
+    for i in 0..100 {
+        iv[i] = i;
+    }
+    let mut direction: Vec<usize> = vec![0; 100];
+
+    let indicies = iv.as_mut_slice();
 
     while win.update() && !win.input.hit(three::KEY_ESCAPE) {
+        let mut rng = thread_rng();
+        indicies.shuffle(&mut rng);
+
         // if let Some(diff) = win.input.timed(three::AXIS_LEFT_RIGHT) {
         let buffer = receiver.recv().expect("unable to get audio buffer");
         let v = buffer.to_vec();
-        let signal = signal::from_iter(v.iter().cloned());
-        let mut rms_signal = signal.rms(ring_buffer);
-        let rms_signal = rms_signal.next();
         let mut spectrograph = sonogram::SpecOptionsBuilder::new(1, 1024)
             .load_data_from_memory(v, mic.default_sample_rate as u32)
             .build();
@@ -167,8 +115,6 @@ fn main() {
         spectrograph.compute(1024, 0.1);
         
         let bins = spectrograph.create_in_memory(false);
-        spectrograph.save_as_png(&std::path::Path::new("test.png"), true).expect("didnt work");
-        // println!("{}", rms_signal);
         let vertex_count = dynamic.vertex_count();
         {
             let mut vmap = win.factory.map_vertices(&mut dynamic);
@@ -178,8 +124,24 @@ fn main() {
                 if ratio < 0.0 {
                     ratio = 0.0 ;
                 }
+                if i % 10 == 0 {
+                    let random_index = indicies[i/10];
+                    noise[random_index] = noise[random_index]/1.2; // (ratio - 0.1)* 10.0;
+                    if direction[random_index] == 0 {
+                        noise[random_index] = noise[random_index] - ratio*6.0;
+                    } else {
+                        noise[random_index] = noise[random_index] + ratio*6.0;
+                    }
+                    if noise[random_index] > 2.0 {
+                        direction[random_index] = 0;
+                    } else if noise[random_index] < -2.0 {
+                        direction[random_index] = 1;
+                    }
+                    // println!("{:?}", noise);
+                    
+                }
                 for j in 0..WIDTH {
-                    frame_buffer[v_i*WIDTH + j] = from_u8_rgb((ratio * 255 as f32) as u8, 0, 0);
+                    freq_frame_buffer[v_i*WIDTH + j] = from_u8_rgb((ratio * 255 as f32) as u8, 0, 0);
                 }
                 let dir = cgmath::Vector4::from(vmap[v_i].pos).truncate();
                 let pos = cgmath::Point3::from_vec(1.0 * dir);
@@ -187,22 +149,57 @@ fn main() {
                 vmap[v_i].pos = [pos.x, pos.y, pos.z, 1.0/ratio.powf(3.0)];
             }
         }
-        window
-            .update_with_buffer(&frame_buffer, WIDTH, HEIGHT)
+
+
+        run_gan_and_fill_buffer(&noise, &model, &mut gan_frame_buffer);
+
+        gan_window
+            .update_with_buffer(&gan_frame_buffer, 512, 512)
             .unwrap();
 
-        angle += cgmath::Rad(1.5 * rms_signal *100.0);
-        // point_light.set_position([1.0* rms_signal *100000.0, 35.0, 35.0]);
-        let q1 = cgmath::Quaternion::from_angle_y(angle);
-        let q2= cgmath::Quaternion::from_angle_y(angle);
-        let q3 = cgmath::Quaternion::from_angle_y(angle);
-        let q4 = cgmath::Quaternion::from_angle_y(angle);
+        freq_window
+            .update_with_buffer(&freq_frame_buffer, WIDTH, HEIGHT)
+            .unwrap();
 
-        mbox.set_orientation(q1);
-        mcyl.set_orientation(q2);
-        msphere.set_orientation(q3);
-        mline.set_orientation(q4);
-        // }
+
         win.render(&cam);
+    }
+}
+
+fn run_gan_and_fill_buffer(noise: &Vec<f32>, gan_model: &tch::CModule, gan_frame_buffer: &mut Vec<u32>) {
+    let mut noise_tensor = tch::Tensor::of_slice(noise);
+    noise_tensor = noise_tensor.unsqueeze(0);
+    noise_tensor = noise_tensor.unsqueeze(2);
+    noise_tensor = noise_tensor.unsqueeze(3);
+    // let t = tch::Tensor::randn(&[1,100,1,1], (tch::Kind::Float, tch::Device::Cpu));
+    let output = gan_model.forward_ts(&[noise_tensor]).expect("could not do forward pass").squeeze().permute(&[1, 2, 0]);
+    let tensor_vec = Vec::<f64>::from((output + 1) * 0.5);
+    for i in 0..128 {
+        for j in 0..128 {
+            let pixel = from_u8_rgb(
+                (tensor_vec[i*128*3+j*3+0]* 255 as f64) as u8,
+                (tensor_vec[i*128*3+j*3+1]* 255 as f64) as u8,
+                (tensor_vec[i*128*3+j*3+2]* 255 as f64) as u8);
+
+            gan_frame_buffer[(i*512*4 + j*4) as usize] = pixel;
+            gan_frame_buffer[(i*512*4 + j*4+1) as usize] = pixel;
+            gan_frame_buffer[(i*512*4 + j*4+2) as usize] = pixel;
+            gan_frame_buffer[(i*512*4 + j*4+3) as usize] = pixel;
+
+            gan_frame_buffer[(i*512*4+(512*1) + j*4) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*1) + j*4+1) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*1) + j*4+2) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*1) + j*4+3) as usize] = pixel;
+
+            gan_frame_buffer[(i*512*4+(512*2) + j*4) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*2) + j*4+1) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*2) + j*4+2) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*2) + j*4+3) as usize] = pixel;
+            
+            gan_frame_buffer[(i*512*4+(512*3) + j*4) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*3) + j*4+1) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*3) + j*4+2) as usize] = pixel;
+            gan_frame_buffer[(i*512*4+(512*3) + j*4+3) as usize] = pixel;
+        }
     }
 }
